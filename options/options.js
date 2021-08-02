6748f2dca5ed24ac4949c201cd3bdadd81c026db
Crowdbotics <team@crowdbotics.com>
2021-08-02 23:43:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "proud_smoke_29357Identifier",
+  url: "https://proud_smoke_29357Identifier.botics.co",
+  api: "https://proud_smoke_29357Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
